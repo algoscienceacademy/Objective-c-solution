8bd9bdb1d0bf17129a9863772406b92c34c7e724
Shahrear Hossain <sshahrearhossain@gmail.com>
2024-10-15 12:24:55 +0600

    update

--- Objective-c-solution/solution11.m
+++ Objective-c-solution/solution11.m
@@ -1,6 +1,39 @@
 //
-//  solution2.m
+//  solution11.m
 //  Objective-c-solution
 //
-//  Created by Shahrear Hossain on 9/10/24.
+//  Created by Shahrear Hossain on 15/10/24.
 //
+
+//#import <Foundation/Foundation.h>
+//
+//int main(int argc, const char* argv[])
+//{
+//    int n = 1;
+//    
+//    do {
+//        NSLog(@" %i squared is %i",n,n*n);
+//        n++;
+//    } while (n <= 12);
+//    
+//    return 0;
+//}
+
+/*
+ output :
+ 1 squared is 1
+ 2 squared is 4
+ 3 squared is 9
+ 4 squared is 16
+ 5 squared is 25
+ 6 squared is 36
+ 7 squared is 49
+ 8 squared is 64
+ 9 squared is 81
+ 10 squared is 100
+ 11 squared is 121
+ 12 squared is 144
+Program ended with exit code: 0
+ 
+ */
+
