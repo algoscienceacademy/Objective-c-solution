e1e449976a9be78c2a93f29137aa7761f6094bad
Shahrear Hossain <sshahrearhossain@gmail.com>
2024-10-15 11:41:32 +0600

    fix solution_9

--- Objective-c-solution/solution8.m
+++ Objective-c-solution/solution8.m
@@ -3,24 +3,24 @@
 //  Objective-c-solution
 //
 //  Created by Shahrear Hossain on 15/10/24.
-#import <Foundation/Foundation.h>
-
-int main (int argc, const char* argv [] )
-{
-    @autoreleasepool {
-        int year;
-        NSLog(@"Enter a year: ");
-        scanf("%i", &year);
-        
-        if (( year % 400 == 0 || year % 4 == 0) && year % 100 != 0)
-        {
-            NSLog(@" %i is leap year",year);
-            
-        }
-        else {
-            NSLog(@" %i is not a leap year",year);
-        }
-        return 0;
-        
-    }
-}
+//#import <Foundation/Foundation.h>
+//
+//int main (int argc, const char* argv [] )
+//{
+//    @autoreleasepool {
+//        int year;
+//        NSLog(@"Enter a year: ");
+//        scanf("%i", &year);
+//        
+//        if (( year % 400 == 0 || year % 4 == 0) && year % 100 != 0)
+//        {
+//            NSLog(@" %i is leap year",year);
+//            
+//        }
+//        else {
+//            NSLog(@" %i is not a leap year",year);
+//        }
+//        return 0;
+//        
+//    }
+//}
--- Objective-c-solution/solution9.m
+++ Objective-c-solution/solution9.m
@@ -1,6 +1,28 @@
 //
-//  solution2.m
+//  solution9.m
 //  Objective-c-solution
 //
 //  Created by Shahrear Hossain on 9/10/24.
 //
+
+// check wheither is voewl or not
+
+#import <Foundation/Foundation.h>
+
+int main(int argc , const char* argv[])
+{
+    char ch;
+    NSLog(@"Enter a Alpabet :");
+    scanf("%c",&ch);
+    
+    if (ch =='A' || ch == 'E' ||ch == 'I' || ch== 'O'|| ch =='U' || ch <= 'a' ||ch == 'e' || ch== 'i'|| ch =='o' || ch <= 'u')
+    {
+        NSLog(@"%c is a Vowel",ch);
+    }
+    else 
+    {
+        NSLog(@"%c is  not  Vowel",ch);
+        
+    }
+    return 0;
+}
