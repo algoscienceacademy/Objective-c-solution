be27257162a6f582d032d4ffb49c968ce4d596d2
Shahrear Hossain <sshahrearhossain@gmail.com>
2024-10-15 12:11:54 +0600

    add 10

--- Objective-c-solution/solution10.m
+++ Objective-c-solution/solution10.m
@@ -1,6 +1,33 @@
 //
-//  solution2.m
+//  solution10.m
 //  Objective-c-solution
 //
-//  Created by Shahrear Hossain on 9/10/24.
+//  Created by Shahrear Hossain on 15/10/24.
 //
+
+// Reverse a number without use recursion
+
+//#import <Foundation/Foundation.h>
+//
+//int main (int argc, const char* argv[])
+//{
+//    @autoreleasepool {
+//        int rev = 0;
+//        int temp ;
+//        int a;
+//        
+//        NSLog(@"Enter a number ");
+//        scanf("%i", &a);
+//        temp = a;
+//    
+//        
+//        while( temp != 0)
+//        {
+//            rev = rev * 10 + temp % 10;
+//            
+//            temp /= 10;
+//        }
+//        NSLog(@" %i is a reverse %i",a,rev);
+//    }
+//    return 0;
+//}
--- Objective-c-solution/solution9.m
+++ Objective-c-solution/solution9.m
@@ -7,22 +7,22 @@
 
 // check wheither is voewl or not
 
-#import <Foundation/Foundation.h>
-
-int main(int argc , const char* argv[])
-{
-    char ch;
-    NSLog(@"Enter a Alpabet :");
-    scanf("%c",&ch);
-    
-    if (ch =='A' || ch == 'E' ||ch == 'I' || ch== 'O'|| ch =='U' || ch <= 'a' ||ch == 'e' || ch== 'i'|| ch =='o' || ch == 'u')
-    {
-        NSLog(@"%c is a Vowel",ch);
-    }
-    else 
-    {
-        NSLog(@"%c is  not  Vowel",ch);
-        
-    }
-    return 0;
-}
+//#import <Foundation/Foundation.h>
+//
+//int main(int argc , const char* argv[])
+//{
+//    char ch;
+//    NSLog(@"Enter a Alpabet :");
+//    scanf("%c",&ch);
+//    
+//    if (ch =='A' || ch == 'E' ||ch == 'I' || ch== 'O'|| ch =='U' || ch <= 'a' ||ch == 'e' || ch== 'i'|| ch =='o' || ch == 'u')
+//    {
+//        NSLog(@"%c is a Vowel",ch);
+//    }
+//    else 
+//    {
+//        NSLog(@"%c is  not  Vowel",ch);
+//        
+//    }
+//    return 0;
+//}
